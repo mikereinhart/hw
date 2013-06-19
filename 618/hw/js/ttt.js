e2db8b54b75698c5d9716fbc814ff16025d81660
Michael Reinhart <mreinhart88@gmail.com>
2013-06-19 11:10:21 -0400

    got the reset button to work and show when the game is over. the only issue is the reset button is present upone page refresh, then only dissappears after the initial turn is made

--- 618/hw/js/ttt.js
+++ 618/hw/js/ttt.js
@@ -3,22 +3,18 @@ This way, we can take the absolute value of 3 positions and
 check to see it equals 3. Any other combination besides 3 of either
 X or O will not equal 3, making it a safe way to determine a winner.
 */
-
+var game_finished = false;
 var turn = 1;
 var board = [
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0]
       ];
-
-var game_finished = false;
-
 function check_all() {
   check_rows();
   check_columns();
   check_diag();
 }
-
 function check_rows() {
   if (game_finished === false) {
     var length = board.length;
@@ -28,7 +24,6 @@ function check_rows() {
       }
   }
 }
-
 function check_columns() {
   if (game_finished === false) {
     var length = board.length;
@@ -38,9 +33,8 @@ function check_columns() {
       }
   }
 }
-
 function check_diag() {
-  if (game_finished === false) {
+  if (!game_finished) {
     var length = board.length;
     if (Math.abs(board[0][0] + board[1][1] + board[2][2]) === 3) {
       game_over();
@@ -50,39 +44,46 @@ function check_diag() {
     }
   }
 }
-
 function box_click() {
+  if (!game_finished) {
+  $('#reset_button').hide();
   var box_id = $(this).attr('id'); //Ex. id='box_0_0'
-
   row = parseInt(box_id.slice(4, 5));
   column = parseInt(box_id.slice(6, 7));
-
   if (turn % 2 === 0) {
     board[row][column] = 1;
     $(this).addClass('positive');
-  }
-  else {
+  } else {
     board[row][column] = -1;
     $(this).addClass('negative');
   }
   turn ++;
   check_all();
-  if (turn === 10) {
+  if ((turn > 9) && (!game_finished)) {
+    $('#reset_button').show();
     alert('Nobody won! How typical!');
+    }
   }
 }
-
 function game_over() {
+  $('#reset_button').show();
   alert("You've won! The game is over!");
   game_finished = true;
 }
-
+function reset_game() {
+  board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+      ];
+  game_finished = false;
+  turn = 1;
+  $('.block').removeClass('positive');
+  $('.block').removeClass('negative');
+  $('#reset_button').hide();
+}
 
 $(document).ready(function() {
   $('.block').click(box_click);
+  $('#reset_button').click(reset_game);
 });
-
-
-
-
-
