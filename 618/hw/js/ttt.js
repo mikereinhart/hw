20fbf74afd1f579be5e6a84ad8acd2c3d548cf3a
Michael Reinhart <mreinhart88@gmail.com>
2013-06-18 20:32:41 -0400

    Working on the 618 homework now. I can produce colors and I have the game end with a function that blocks other functions and produces an alert.

---
+++ 618/hw/js/ttt.js
@@ -0,0 +1,84 @@
+/*Players will be X and O. x = 1 and o = -1. 
+This way, we can take the absolute value of 3 positions and
+check to see it equals 3. Any other combination besides 3 of either
+X or O will not equal 3, making it a safe way to determine a winner.
+*/
+
+var turn = 1;
+var board = [
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 0, 0]
+      ];
+
+var game_finished = false;
+
+
+function check_rows() {
+  if (game_finished === false) {
+    var length = board.length;
+    for(r=0; r<length; r++)
+      if (Math.abs(board[r][0] + board[r][1] + board[r][2]) === 3) {
+        game_over();
+      }
+  }
+}
+
+function check_columns() {
+  if (game_finished === false) {
+    var length = board.length;
+    for(c=0; c<length; c++)
+      if (Math.abs(board[0][c] + board[1][c] + board[2][c]) === 3) {
+        game_over();
+      }
+  }
+}
+
+function check_diag() {
+  if (game_finished === false) {
+    var length = board.length;
+    if (Math.abs(board[0][0] + board[1][1] + board[2][2]) === 3) {
+      game_over();
+    }
+    if (Math.abs(board[0][2] + board[1][1] + board[2][0]) === 3) {
+      game_over();
+    }
+  }
+}
+
+function check_win() {
+  row_check();
+  column_check();
+  diagonal_check();
+}
+
+function box_click() {
+  var box_id = $(this).attr('id'); //Ex. id='box_0_0'
+
+  row = parseInt(box_id.slice(4, 5));
+  column = parseInt(box_id.slice(6, 7));
+
+  if (turn % 2 === 0) {
+    board[row][column] = 1;
+    $(this).addClass('positive');
+  } else {
+    board[row][column] = -1;
+    $(this).addClass('negative');
+  }
+  turn ++;
+}
+
+function game_over() {
+  alert("You've won! The game is over!");
+  game_finished = true;
+}
+
+
+$(document).ready(function() {
+  $('.block').click(box_click);
+});
+
+
+
+
+
---
+++ 618/in_class_ex/js/tictactoe.js
@@ -0,0 +1,6 @@
+var player = '';
+
+var tic_tac_toe array = [['a', 'b', 'c'],
+                         ['a', 'b', 'c'],
+                         ['a', 'b', 'c']
+                        ];
